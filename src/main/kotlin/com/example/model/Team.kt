22dc213faba66bfeab2aca2168d9c04f9fa36a79
Guillem Mercadal <guillem.mercadal@gmail.com>
2019-05-14 20:40:32 +0200

    Added example post

---
+++ src/main/kotlin/com/example/model/Team.kt
@@ -0,0 +1,10 @@
+package com.example.model
+
+class Team() {
+    lateinit var name: String
+
+    constructor(
+            name: String): this() {
+        this.name = name
+    }
+}
---
+++ src/main/kotlin/com/example/model/TeamMember.kt
@@ -0,0 +1,10 @@
+package com.example.model
+
+class TeamMember() {
+    lateinit var name: String
+
+    constructor(
+            name: String): this() {
+        this.name = name
+    }
+}
