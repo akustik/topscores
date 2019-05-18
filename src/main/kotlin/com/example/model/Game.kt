5c4d19d23672c0335f6a58a0263088f72002317a
Guillem Mercadal <guillem.mercadal@gmail.com>
2019-05-18 10:33:58 +0200

    Added creation time automatically

--- src/main/kotlin/com/example/model/Game.kt
+++ src/main/kotlin/com/example/model/Game.kt
@@ -4,11 +4,14 @@ package com.example.model
 class Game() {
     lateinit var account: String
     lateinit var parties: List<Party>
+    var timestamp: Long = System.currentTimeMillis()
 
     constructor(
             account: String,
-            parties: List<Party>): this() {
+            parties: List<Party>,
+            timestamp: Long): this() {
         this.account = account
         this.parties = parties
+        this.timestamp = timestamp
     }
 }
--- src/test/kotlin/com/example/WebMockTest.kt
+++ src/test/kotlin/com/example/WebMockTest.kt
@@ -1,6 +1,5 @@
 package com.example
 
-import org.hamcrest.Matchers.containsString
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 
 
 @WebMvcTest(Controller::class)
@@ -29,7 +28,10 @@ class WebMockTest {
         val patxanga = WebMockTest::class.java.getResource("/samples/patxanga.json").readText()
         var request = post("/games/add").content(patxanga).contentType("application/json")
         this.mockMvc!!.perform(request).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("\"account\":\"patxanga\"")))
+                .andExpect(content().json(patxanga, true))
+        
+        
     }
-
 }
+
+
