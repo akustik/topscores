110ed2223b5329be9c2229705fa0b833a8eec9a4
Guillem Mercadal <guillem.mercadal@gmail.com>
2019-06-27 22:35:21 +0200

    Add dry run simulation flag to the addgame command

--- src/main/kotlin/org/gmd/command/AddGame.kt
+++ src/main/kotlin/org/gmd/command/AddGame.kt
@@ -3,6 +3,9 @@ package org.gmd.command
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import org.gmd.Algorithm
 import org.gmd.model.Game
 import org.gmd.model.Party
 import org.gmd.model.Team
@@ -12,6 +15,7 @@ import org.gmd.slack.SlackResponseHelper
 
 class AddGame(val response: SlackResponseHelper, val service: GameService, val account: String, val tournament: String) : CliktCommand(help = "Add a new game", printHelpOnEmptyArgs = true) {
     val players by argument(help = "Ordered list of the scoring of the event, i.e: winner loser").multiple(required = true)
+    val dryRun by option(help = "Returns an ELO simulation without actually storing the game").flag()
     override fun run() {
         val normalizedPlayers = players.map { p -> p.toLowerCase() }
         val parties = normalizedPlayers.reversed().mapIndexed { index, player ->
@@ -30,14 +34,32 @@ class AddGame(val response: SlackResponseHelper, val service: GameService, val a
                 timestamp = System.currentTimeMillis()
         )
 
-        val storedGame = service.addGame(account, gameToCreate)
-        val storedPlayers = storedGame.parties
-                .sortedByDescending { party -> party.score }
-                .flatMap { p -> p.members.map { m -> m.name } }
+        if(dryRun) {
+            val simulation = service.computeTournamentMemberScoreEvolution(
+                    account = account,
+                    tournament = tournament,
+                    player = normalizedPlayers,
+                    alg = Algorithm.ELO,
+                    withGames = listOf(gameToCreate))
 
-        val scores = storedPlayers.mapIndexed { index, s -> "${index + 1}. $s" }.joinToString(separator = "\n")
+            val eloUpdate = simulation
+                    .map { e -> Pair(e.member, e.score.last() - e.score.dropLast(1).last()) }
+                    .sortedByDescending { p -> p.second }
 
-        response.publicMessage("Good game! A new game entry has been created!",
-                listOf(scores))
+            val ratings = eloUpdate.mapIndexed { index, s -> "${index + 1}. ${s.first} (${s.second})" }.joinToString(separator = "\n")
+
+            response.publicMessage("Find the ELO simulation below, is it worth the risk?",
+                    listOf(ratings))
+        } else {
+            val storedGame = service.addGame(account, gameToCreate)
+            val storedPlayers = storedGame.parties
+                    .sortedByDescending { party -> party.score }
+                    .flatMap { p -> p.members.map { m -> m.name } }
+
+            val scores = storedPlayers.mapIndexed { index, s -> "${index + 1}. $s" }.joinToString(separator = "\n")
+
+            response.publicMessage("Good game! A new game entry has been created!",
+                    listOf(scores))
+        }
     }
 }
