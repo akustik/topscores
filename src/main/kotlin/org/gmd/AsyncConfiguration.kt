08218ec3bfa28f8e6d44fa197b5bc28ef8474895
Guillem Mercadal <guillem.mercadal@gmail.com>
2019-07-02 19:16:34 +0200

    Fix async execution

--- src/main/kotlin/org/gmd/AsyncConfiguration.kt
+++ src/main/kotlin/org/gmd/AsyncConfiguration.kt
@@ -4,16 +4,20 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
-import java.util.concurrent.Executor
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
 
 
 @Configuration
 @EnableAsync
 open class AsyncConfiguration : AsyncConfigurer {
-    
+
     override fun getAsyncExecutor(): Executor {
-        return ThreadPoolTaskExecutor()
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 1
+        executor.maxPoolSize = 10
+        executor.initialize()
+        return executor
     }
 
     override fun getAsyncUncaughtExceptionHandler(): AsyncUncaughtExceptionHandler {
--- src/main/kotlin/org/gmd/CustomAsyncExceptionHandler.kt
+++ src/main/kotlin/org/gmd/CustomAsyncExceptionHandler.kt
@@ -1,5 +1,6 @@
 package org.gmd
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
 import java.lang.reflect.Method
@@ -11,7 +12,9 @@ open class CustomAsyncExceptionHandler : AsyncUncaughtExceptionHandler {
     
     override fun handleUncaughtException(
             throwable: Throwable, method: Method, vararg obj: Any) {
-        logger.error("Unable to perform async execution with method ${method.name} and args $obj", throwable)
+        val mapper = ObjectMapper()
+        val arguments = obj.map { a -> mapper.writeValueAsString(a) }
+        logger.error("Unable to perform async execution with method ${method.name} and args $arguments", throwable)
     }
 
 }
