19e977ccdb9b08b4e79630f28a6b7d9d0d1b4232
francisperez <francisperez@scopely.com>
2019-07-10 15:58:47 +0200

    fix matching

--- src/main/kotlin/org/gmd/service/GameService.kt
+++ src/main/kotlin/org/gmd/service/GameService.kt
@@ -11,18 +11,20 @@ import java.time.Instant
 interface GameService {
 
     fun listGames(account: String, maxElements: Int = 5): List<Game>
-    
+
+    fun listGames(account: String, tournament: String, maxElements: Int = 5): List<Game>
+
     fun addGame(account: String, game: Game): Game
 
     fun computeTournamentMemberScores(account: String, tournament: String, alg: Algorithm = Algorithm.SUM, teams: List<String> = emptyList()): List<Score>
-    
+
     fun computeTournamentMemberScoreEvolution(account: String, tournament: String, player: List<String>, alg: Algorithm = Algorithm.SUM, withGames: List<Game> = emptyList()): List<Evolution>
 
     fun computeTournamentMemberMetrics(account: String, tournament: String): List<MemberMetrics>
-    
+
     fun listTournaments(account: String): List<String>
-    
+
     fun listEntries(account: String, tournament: String, maxElements: Int = 5): List<Pair<Instant, Game>>
-    
+
     fun deleteEntry(account: String, tournament: String, createdAt: Instant): Boolean
 }
--- src/test/kotlin/org/gmd/command/MatchUpTest.kt
+++ src/test/kotlin/org/gmd/command/MatchUpTest.kt
@@ -5,6 +5,7 @@ import org.gmd.model.Party
 import org.gmd.model.Team
 import org.gmd.model.TeamMember
 import org.gmd.service.GameService
+import org.gmd.slack.SlackResponse
 import org.gmd.slack.SlackResponseHelper
 import org.junit.Assert
 import org.junit.Test
@@ -13,6 +14,8 @@ import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -49,26 +52,45 @@ class MatchUpTest {
 
     @Test
     fun testSlackResponse() {
-        val helper = SlackResponseHelper { run {} }
 
-        Mockito.`when`(gameService.listGames(account, 1000)).thenReturn(listOf(addedGame))
+        val completableFuture = CompletableFuture<SlackResponse>()
+        val matchUp = createTestMatchUp(completableFuture)
 
+        matchUp.parse(listOf(player1, player2))
 
-        MatchUp(helper, gameService, account, tournament, "player").parse(listOf(player2, player1))
+        val response = completableFuture.get(1, TimeUnit.MINUTES)
+        Assert.assertEquals("From 1 games player1 beats player2 a 100%.", response.text)
+    }
 
-        Assert.assertEquals("From 1 games player2 beats player1 a 100%.", helper.slackResponse.text)
+    @Test
+    fun testSlackResponseOnWrongTournament() {
+        val completableFuture = CompletableFuture<SlackResponse>()
+        val matchUp = createTestMatchUp(completableFuture, "a fake tournament")
+
+        matchUp.parse(listOf(player1, player2))
+
+        val response = completableFuture.get(1, TimeUnit.MINUTES)
+        Assert.assertEquals("No matches found", response.text)
     }
 
     @Test
     fun testSlackResponseOnUnMatching() {
-        val helper = SlackResponseHelper { run {} }
+        val completableFuture = CompletableFuture<SlackResponse>()
+        val matchUp = createTestMatchUp(completableFuture)
 
         Mockito.`when`(gameService.listGames(account, 1000)).thenReturn(listOf(addedGame))
 
+        matchUp.parse(listOf(player1, "unknown player"))
 
-        MatchUp(helper, gameService, account, tournament, "player").parse(listOf(player1, "unknown player"))
+        val response = completableFuture.get(1, TimeUnit.MINUTES)
+        Assert.assertEquals("No matches found", response.text)
+    }
 
-        Assert.assertEquals("No matches found", helper.slackResponse.text)
+    private fun createTestMatchUp(completableFuture: CompletableFuture<SlackResponse>, matchUpTournament: String = tournament): MatchUp {
+        val helper = SlackResponseHelper { response -> completableFuture.complete(response) }
+
+        Mockito.`when`(gameService.listGames(account, tournament, 1000)).thenReturn(listOf(addedGame))
+        return MatchUp(helper, gameService, account, matchUpTournament, "player")
     }
 }
 
