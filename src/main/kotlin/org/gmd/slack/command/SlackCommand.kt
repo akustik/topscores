353caa442aa1cf79efb3acb73704e9a070c44d25
Guillem Mercadal <guillem.mercadal@gmail.com>
2019-10-22 18:40:30 +0200

    Do support double quotes in the parameters

--- src/main/kotlin/org/gmd/slack/command/SlackCommand.kt
+++ src/main/kotlin/org/gmd/slack/command/SlackCommand.kt
@@ -5,15 +5,26 @@ import org.gmd.Algorithm
 interface SlackCommand {
 
     fun normalizePlayers(players: List<String>): List<List<String>> {
-        val argString = players
-                .joinToString(" ").toLowerCase()
-        val teamString = argString.split(",", ".", "|")
-        return if(teamString.size == 1) {
-            teamString.first().split(" ").map { t -> listOf(t) }
+        return partition(players, emptyList())
+    }
+
+    private tailrec fun partition(players: List<String>, acc: List<List<String>>): List<List<String>> {
+        return if(players.isEmpty()) {
+            acc
         } else {
-            teamString.map { t -> t.trim().split(" ") }
+            val idx = players.indexOf(",")
+            if(idx >= 0) {
+                val remaining = players.subList(idx + 1, players.size)
+                val team = players.subList(0, idx)
+                partition(remaining, acc + listOf(team))
+            } else if(acc.isEmpty()) {
+                partition(emptyList(), players.map { p -> listOf(p) })
+            } else {
+                partition(emptyList(), acc + listOf(players))
+            }
         }
     }
+
 
     fun parseAlgorithm(alg: String): Algorithm {
         return Algorithm.valueOf(alg.toUpperCase())
