1f9bce22d07a33d6616f7626eb45b841ae4e3fb3
Guillem Mercadal <guillem.mercadal@gmail.com>
2019-10-22 18:47:13 +0200

    Do support double quotes

--- src/main/kotlin/org/gmd/slack/command/SlackCommand.kt
+++ src/main/kotlin/org/gmd/slack/command/SlackCommand.kt
@@ -9,20 +9,24 @@ interface SlackCommand {
     }
 
     private tailrec fun partition(players: List<String>, acc: List<List<String>>): List<List<String>> {
-        return if(players.isEmpty()) {
+        return if (players.isEmpty()) {
             acc
         } else {
-            val idx = players.indexOf(",")
-            if(idx >= 0) {
+            val idx = idxForSeparator(players)
+            if (idx >= 0) {
                 val remaining = players.subList(idx + 1, players.size)
                 val team = players.subList(0, idx)
                 partition(remaining, acc + listOf(team))
-            } else if(acc.isEmpty()) {
+            } else if (acc.isEmpty()) {
                 partition(emptyList(), players.map { p -> listOf(p) })
             } else {
                 partition(emptyList(), acc + listOf(players))
             }
         }
+    }
+
+    private fun idxForSeparator(players: List<String>): Int {
+        return maxOf(players.indexOf(","), players.indexOf("."), players.indexOf("|"))
     }
 
 
